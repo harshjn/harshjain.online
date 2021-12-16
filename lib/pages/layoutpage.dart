7f3861a17170e033b8492ac6b3a378f914d05c68
Harsh Jain <harshjainldh@gmail.com>
2021-12-16 16:29:40 +0530

    Update layoutpage.dart

--- lib/pages/layoutpage.dart
+++ lib/pages/layoutpage.dart
@@ -3,7 +3,7 @@ import 'package:DeveloperFolio/configure/centeredview.dart';
 import 'package:DeveloperFolio/configure/navigation_service.dart';
 import 'package:DeveloperFolio/include/navbar/drawer/drawernav.dart';
 import 'package:DeveloperFolio/include/navbar/navigationbar.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide NavigationBar;
 import 'package:responsive_builder/responsive_builder.dart';
 import 'package:DeveloperFolio/configure/routing.dart';
 
